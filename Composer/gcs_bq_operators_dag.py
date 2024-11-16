3b7ebc7f70d4629cc0bac58b34c8876635f1547c
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-11-16 13:27:28 -0600

    Update gcs_bq_operators_dag.py

--- Composer/gcs_bq_operators_dag.py
+++ Composer/gcs_bq_operators_dag.py
@@ -13,7 +13,7 @@ yesterday = datetime.combine(datetime.today() - timedelta(1), datetime.min.time(
 default_arg = {
     'start_date' : yesterday,
     'email_on_failure' : True,
-    'email_on_retry' : False,
+    'email_on_retry' : True,
     'retries' : 1,
     'retry_delay' : timedelta(minutes=5) # 5 mins delay to retry.
 }
